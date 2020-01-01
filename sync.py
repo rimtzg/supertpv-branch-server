2202f73dec2bc4d8e09185c0daa13dd433013363
Ricardo Martinez <rimtzg@gmail.com>
2020-01-01 13:39:16 -0600

    App config added

--- sync.py
+++ sync.py
@@ -1,1 +1,12 @@
-print("Hello")
+import configparser
+import os
+
+if(os.environ.get('SNAP_COMMON')):
+    SNAP_COMMON = os.environ['SNAP_COMMON']
+else:
+    SNAP_COMMON = ''
+
+app_config = configparser.ConfigParser()
+app_config.read( os.path.join( SNAP_COMMON, 'app_config.ini' ) )
+
+print(app_config['API']['URL'])
