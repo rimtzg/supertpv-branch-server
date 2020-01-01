00bb71bf2b8353d78add0b9ad1af1307c1cc5f7d
Ricardo Martinez <rimtzg@gmail.com>
2020-01-01 15:32:31 -0600

    Update sync.py

--- sync.py
+++ sync.py
@@ -2,7 +2,7 @@ import configparser
 import os
 
 if(os.environ.get('SNAP_COMMON')):
-    SNAP_COMMON = os.environ['SNAP_COMMON']
+    SNAP_COMMON = os.environ['SNAP_USER_COMMON']
 else:
     SNAP_COMMON = ''
 
