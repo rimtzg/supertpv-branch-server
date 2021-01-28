0c2636541c8ef2aa3c77e8725552e065edf41b8d
Ricardo Martinez <rimtzg@gmail.com>
2021-01-28 10:20:11 -0600

    Bug fixes

--- schemas/orders.py
+++ schemas/orders.py
@@ -10,7 +10,7 @@ schema = Schema({
     'client_id'     : Use(ObjectId),
     'client_name'   : Use(str.lower),
     'date'          : Use(datetime.fromisoformat),
-
+    'number'        : Use(int),
     Optional('active', default=True) : Use(bool),
     
     'categories' : [
@@ -23,6 +23,7 @@ schema = Schema({
                     'code'   : Use(str.lower),
                     'name'   : Use(str.lower),
                     'amount' : Or(Use(int), Use(float)),
+                    'label'  : Use(str.lower),
                     'price'  : Or(Use(int), Use(float))
                 }
             ]
