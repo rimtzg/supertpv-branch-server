c8ebf8b1d43c90d964a3f26c195b7078a573adb7
Ricardo Martinez <rimtzg@gmail.com>
2021-05-31 22:35:25 -0500

    Filter orders by date

--- methods/api.py
+++ methods/api.py
@@ -580,8 +580,24 @@ class Methods():
         return document
 
     def get_orders(self):
+        args = request.args
+        start = datetime.utcnow().replace(hour=0, minute=0, second=0)
+        end = datetime.utcnow().replace(hour=23, minute=59, second=59)
+
+
+        if(args.get('start') ):
+            start = datetime.strptime(args['start'], '%Y-%m-%d').replace(hour=0, minute=0, second=0)
+
+        if(args.get('end') ):
+            end = datetime.strptime(args['end'], '%Y-%m-%d').replace(hour=23, minute=59, second=59)
+
         query = {
             'active' : True
+        }
+
+        query['date'] = {
+            '$gte' : start,
+            '$lt' : end
         }
 
         documents = mongo['orders'].find(query).sort([("date", 1)])
